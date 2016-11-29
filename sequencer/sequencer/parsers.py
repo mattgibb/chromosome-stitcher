483cbd4c2441ee057ebdb3b2ecc5a6acd37087a4
Matt Gibb <matt@mattgibb.com>
2016-11-29 10:22:44 -0800

    Parse FASTA file

---
+++ sequencer/sequencer/parsers.py
@@ -0,0 +1,22 @@
+import re
+from collections import namedtuple
+
+class Sequence(namedtuple('Sequence', 'name bases')):
+    __format = re.compile(r'(?P<name>[^\n]+)\n(?P<bases>[ACGT\n]+)')
+
+    @classmethod
+    def parse(cls, seq_string):
+        seq = cls.__format.fullmatch(seq_string)
+        if not seq: raise NameError("sequence isn't the right format:\n%s" % log_string)
+        name = seq.group('name')
+        bases = seq.group('bases').replace('\n', '')
+        return cls(name, bases)
+
+    def __repr__(self, n=10):
+        ellipsis = '...' if len(self.bases) > n else ''
+        return self.name + ': ' + self.bases[:n] + ellipsis
+
+def parse_fasta(io):
+    paragraphs = io.read().split('>')[1:]
+    return [Sequence.parse(p) for p in paragraphs]
+    
---
+++ sequencer/tests/test_parsers.py
@@ -0,0 +1,34 @@
+import unittest
+import io
+from sequencer.parsers import Sequence, parse_fasta 
+
+class TestParsers(unittest.TestCase):
+    def test_sequence(self):
+        name = "some_fragment"
+        bases = "ACC\nGGTT\n"
+        sequence_string = "%s\n%s" % (name, bases)
+        sequence = Sequence.parse(sequence_string)
+
+        self.assertEqual(name, sequence.name)
+        self.assertEqual(bases.replace("\n", ''), sequence.bases)
+
+    def test_parse_fasta(self):
+        file = io.StringIO(
+""">Frag_56
+ATTAGACCTG
+>Frag_57
+CCTGCCGGAA
+>Frag_58
+AGACCTGCCG
+>Frag_59
+GCCGGAATAC
+""")
+
+        sequences = [
+            ("Frag_56", "ATTAGACCTG"),
+            ("Frag_57", "CCTGCCGGAA"),
+            ("Frag_58", "AGACCTGCCG"),
+            ("Frag_59", "GCCGGAATAC")
+        ]
+
+        self.assertEqual(sequences, parse_fasta(file))
