3e3dc067d71f246e7cc781ebc16e6bdf34efa943
Matt Gibb <matt@mattgibb.com>
2016-11-30 00:12:27 -0800

    Fixed cross platform newlines in FASTA regex parse

--- sequencer/sequencer/parsers.py
+++ sequencer/sequencer/parsers.py
@@ -2,14 +2,14 @@ import re
 from collections import namedtuple
 
 class Sequence(namedtuple('Sequence', 'name bases')):
-    __format = re.compile(r'(?P<name>[^\n]+)\n(?P<bases>[ACGT\n]+)')
+    __format = re.compile(r'(?P<name>[^\r\n]+)\r?\n(?P<bases>[ACGT\r\n]+)')
 
     @classmethod
     def parse(cls, seq_string):
         seq = cls.__format.fullmatch(seq_string)
         if not seq: raise NameError("sequence isn't the right format:\n%s" % log_string)
         name = seq.group('name')
-        bases = seq.group('bases').replace('\n', '')
+        bases = seq.group('bases').replace('\r', '').replace('\n', '')
         return cls(name, bases)
 
     def __repr__(self, n=10):
